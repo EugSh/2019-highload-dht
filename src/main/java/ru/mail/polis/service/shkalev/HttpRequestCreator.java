ada729370eab9247b62fb15f4ddab97291028b37
EugSh <eug.shkalev@yandex.ru>
2019-11-09 23:35:46 +0300

    fix time out

--- src/main/java/ru/mail/polis/service/shkalev/HttpRequestCreator.java
+++ src/main/java/ru/mail/polis/service/shkalev/HttpRequestCreator.java
@@ -14,7 +14,7 @@ public class HttpRequestCreator {
     private final String key;
     private final byte[] body;
     private final int codeStatus;
-    private static final Duration TIME_OUT = Duration.ofSeconds(1);
+    private static final Duration TIME_OUT = Duration.ofMillis(500);
 
     HttpRequestCreator(@NotNull final Replicas rf, @NotNull final ByteBuffer key,
                        @NotNull final Request request, final int codeStatus) {
